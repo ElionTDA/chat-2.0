e7a1aac4530ed4383b9cfda70da93bacb7041d62
Diego Martin Perez <diego.martin.perez5@gmail.com>
2017-04-21 20:40:45 +0200

    Primeras pinceladas, muy básicas.

--- Practica2ChatRMI/src/main/java/es/ubu/lsi/server/CharServerStarter.java
+++ Practica2ChatRMI/src/main/java/es/ubu/lsi/server/CharServerStarter.java
@@ -1,5 +1,34 @@
 package es.ubu.lsi.server;
 
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.server.RMIClassLoader;
+import java.util.Properties;
+
 public class CharServerStarter {
+	
+   	public static void main(String args[]) {
+        // Crea e instala el gestor de seguridad
+        if (System.getSecurityManager() == null) {
+          System.setSecurityManager(new SecurityManager());
+        }
+        
+        try {
+          Properties p = System.getProperties();
+          
+          // lee el goodbyes
+          String url = p.getProperty("java.rmi.server.codebase");
+          
+          // Cargador de clases dinámico ...
+          Class<?> serverclass = RMIClassLoader.loadClass(url, "es.ubu.lsi.Servidor");
+          Naming.rebind("/Servidor", (Remote) serverclass.newInstance());
+          System.out.println("Servidor registrado...");
+        } 
+        catch (Exception e) {
+  		System.err.println("Excepcion recogida: " + e.toString());
+        }
+     }
+   	
+   	
 
 }
